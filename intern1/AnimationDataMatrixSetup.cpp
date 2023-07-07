b257614be2bfcb9386ac6ad477db1e92bbd7ba45
schoe <csh4715824678@gmail.com>
2023-07-07 19:00:25 +0900

    header fix

--- intern1/AnimationDataMatrixSetup.cpp
+++ intern1/AnimationDataMatrixSetup.cpp
@@ -2,7 +2,7 @@
 #include "include/Animation.h"
 #include "include/GLM/gtx/quaternion.hpp"
 #include "include/GLM/gtc/matrix_inverse.hpp"
-
+#include <deque>
 void AnimationDataMatrixSetup::processNode(AnimationData& data)
 {
     for (int i =0; i <data._localRotation.size(); ++i)
