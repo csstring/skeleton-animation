c599ed63d0122d236d3e160cce12f2c8fde3718d
schoe <csh4715824678@gmail.com>
2023-07-14 13:55:49 +0900

    fix

--- intern1/Simulator.cpp
+++ intern1/Simulator.cpp
@@ -69,12 +69,12 @@ void Simulator::draw(void)
         {
             glBindVertexArray(VAO[bone._boneIndex]);
             glm::mat4 toParentDir = _worldTrans * _transForm[bone._boneIndex] * ft_rotate(glm::vec3(0.0,0.0,1.0), bone._direction);// * glm::inverse(test3); 
-            // Cylinder cylinder(0.2, 0.7 *_skeleton.getGBL() * bone._length ,16, toParentDir);
-            // cylinder.initialize(color, VBC[bone._boneIndex]);
-            // cylinder.render(VBO[bone._boneIndex]);
-            Line line(0.7 *_skeleton.getGBL() * bone._length, toParentDir);
-            line.initialize(color, VBC[bone._boneIndex]);
-            line.render(VBO[bone._boneIndex]);
+            Cylinder cylinder(0.2, 0.7 *_skeleton.getGBL() * bone._length ,16, toParentDir);
+            cylinder.initialize(color, VBC[bone._boneIndex]);
+            cylinder.render(VBO[bone._boneIndex]);
+            // Line line(0.7 *_skeleton.getGBL() * bone._length, toParentDir);
+            // line.initialize(color, VBC[bone._boneIndex]);
+            // line.render(VBO[bone._boneIndex]);
             glBindVertexArray(0);
         }
     }
